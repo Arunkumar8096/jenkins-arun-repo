82b30d290089fb9d562c9d5bcfd6d1394069d9b1
ARUN KUMAR <ARUN KUMAR@LAPTOP-5HV40H5E>
2025-04-19 10:50:22 +0530

    hello

---
+++ jenkinsProject/src/test/java/jenkinsProject/projectJenkinsTest.java
@@ -0,0 +1,16 @@
+package jenkinsProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class projectJenkinsTest {
+	@Test
+	public void jenkins() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+		Reporter.log("demo webshop",true);
+	}
+
+}
